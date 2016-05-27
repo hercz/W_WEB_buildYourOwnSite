3de2cccc8c21233d484ea81276d8c2c115167b7f
Herczku Misi <kmisku@gmail.com>
2016-05-27 18:16:15 +0200

    Update lightswitch function

--- subpages/bulbSwitcher/bulbSwitcher.js
+++ subpages/bulbSwitcher/bulbSwitcher.js
@@ -2,12 +2,16 @@
  * Created by herczkumihalybalazs on 2016.05.27..
  */
 function switchbulb() {
-    var bulbImage = document.getElementById("bulbpic")
+    var bulbImage = document.getElementById("bulbpic");
+    var bulbstatus = document.getElementById("bulbStatus");
     if (bulbImage.src.match("bulbOff")) {
-        bulbImage.src = "subpages/bulbSwitcher/bulbOn.jpg"
+        bulbImage.src = "subpages/bulbSwitcher/bulbOn.jpg";
+        document.getElementById("bulbStatus").textContent = "Off";
     }
     else {
-        bulbImage.src = "subpages/bulbSwitcher/bulbOff.jpeg"
+        bulbImage.src = "subpages/bulbSwitcher/bulbOff.jpeg";
+        document.getElementById("bulbStatus").textContent = "On";
+
     }
     
 }
